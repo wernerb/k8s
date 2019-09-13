46dfe70d7a7175e2ff6a36b0e4683a91dda139f9
Eric Nieuwenhuijsen <enieuwenhuijsen@xebia.com>
2019-09-13 09:25:15 +0200

    Allow privileged

--- day1/start_node.sh
+++ day1/start_node.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 IP=$(ip addr show eth1 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
-sudo hyperkube kubelet --pod-manifest-path=/etc/kubernetes/manifests/ --kubeconfig=/var/lib/kubelet/kubeconfig --hostname-override=$IP &
+sudo hyperkube kubelet --pod-manifest-path=/etc/kubernetes/manifests/ --kubeconfig=/var/lib/kubelet/kubeconfig --hostname-override=$IP --allow-privileged=true &
 sudo kube-proxy --master=http://172.17.8.101:8080 --hostname-override=$IP --masquerade-all 
